b5516709bc6f49917cb040b8320a90ff934fd7a1
opezdol <xxblizzxx1000@gmail.com>
2019-01-19 13:30:52 +0200

    add 12

--- src/dsf/Main.java
+++ src/dsf/Main.java
@@ -4,7 +4,7 @@ public class Main {
 public static void main(String[]args){
 
 	volodya volodya228 = new volodya();
-	
+	System.out.println("12");
 	volodya228.setA(4);
 	System.out.println(volodya228.getA());
 }
