11acbb930f4ce41bb35269896060d4bf26505f28
Artem <guglyad@yandex.ru>
2019-01-19 13:17:49 +0200

    created project

---
+++ src/dsf/Main.java
@@ -0,0 +1,11 @@
+package dsf;
+
+public class Main {
+public static void main(String[]args){
+
+	volodya volodya228 = new volodya();
+	
+	volodya228.setA(4);
+	System.out.println(volodya228.getA());
+}
+}
---
+++ src/dsf/volodya.java
@@ -0,0 +1,16 @@
+package dsf;
+
+public class volodya {
+
+	private int a;
+	private String b;
+	
+	public void setA(int x){
+		a=x;
+	}
+	
+	public int getA(){
+    
+    return a;
+	}
+}
